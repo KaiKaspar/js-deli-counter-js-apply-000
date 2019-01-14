0e4258241d821263dfe885c23066cbd00b52dd6f
Kai Shovlin <kai.k.b.shovlin@gmail.com>
2019-01-14 12:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var katzDeliLine = [];
+<<<<<<< HEAD
  function currentLine(x) {
     var line = []
     if (x.length === 0) {
@@ -9,11 +10,23 @@ var katzDeliLine = [];
       }
       line = line.slice(0, line.length-2)
       return "The line is currently: " + line
+=======
+function currentLine (line) {
+  for (let i = 0; i < line.length; i++) {
+      line.push( " "  + [i + 1] + ". " + line[i])
+  }
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+      return "The line is currently: " + line + "."
+>>>>>>> 5c69091cf6d729dbd6f8308b4e1f708cdb10bf1c
     }
 }
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+var ticketNo = 0 
+function takeANumber(katzDeliLine) {
+  ticketNo = ticketNo + 1
+  katzDeliLine.push(ticketNo)
+  return "Welcome, you are number " + katzDeliLine.length + " in line."
 }
  function nowServing(katzDeliLine) {
 	if (katzDeliLine.length === 0) {
